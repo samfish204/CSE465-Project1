ec9ec42f4c81c86305ef395f9773b3b68e2781da
fishe108 <fishe108@ceclnx01-19.csi.miamioh.edu>
2022-03-29 20:47:08 -0400

    Submission

--- challenging.pl
+++ challenging.pl
@@ -1,13 +1,125 @@
+% Samuel Fisher
+% CSE465
+% Project 1
+% Dr. Femiani
+% Due March 28, 2022
 
-%% I will not accept a submission with the T0D0 comments left in place!
+url("https://logic.puzzlebaron.com/pdf/B627ZQ.pdf").
 
-url("TODO: Replace this with the URL of the puzzle").
+solution([
+['January',		'York',		'X-ray',	'Zenmoto'],
+['February',	'Bauer',	'Romeo',	'Jaorm'],
+['March',		'Charles',	'Oscar',	'Quenray'],
+['April',		'Gilbert',	'Foxtrot',	'Nyeos'],
+['May',			'Peters',	'Tango',	'Loald']
+  ]).
 
-solution([["TODO: Replace this with the correct solution to the puzzle!"]]).
+commanders(['York', 'Bauer', 'Charles', 'Gilbert', 'Peters']).
+battalions(['X-ray', 'Romeo', 'Oscar', 'Foxtrot', 'Tango']).
+locations(['Zenmoto', 'Jaorm', 'Quenray', 'Nyeos', 'Loald']).
 
 solve(T) :-
-	% TODO: Implement this to solve the puzzle. 
+    T = [ ['January', C1, B1, L1],
+          ['February', C2, B2, L2],
+          ['March', C3, B3, L3],
+          ['April', C4, B4, L4],
+          ['May', C5, B5, L5]
+        ],
+    battalions(Battalions), permutation(Battalions, [B1, B2, B3, B4, B5]),
+    clue9(T),  % _, B, _
+    commanders(Commanders), permutation(Commanders, [C1, C2, C3, C4, C5]),
+    clue2(T),  % C, B, _
+    clue4(T),  % C, B, _
+    clue6(T),  % C, B, _
+    clue8(T),  % C, B, _
+    clue11(T), % C, B, _
+    locations(Locations), permutation(Locations, [L1, L2, L3, L4, L5]),
+    clue1(T),  % C, B, L
+    clue3(T),  % C, B, L
+    clue5(T),  % _, B, L
+    clue7(T),  % C, B, L
+    clue10(T), % C, B, L
 	true.
+
+number('January', 1).
+number('February', 2).
+number('March', 3).
+number('April', 4).
+number('May', 5).
+
+% 9. Neither the battalion deploying in January nor the 
+% battalion deploying in March is Romeo Battalion..
+clue9(T) :-
+    member(['January', _, B1, _], T),
+    member(['March', _, B2, _], T),
+    B1 \= 'Romeo',
+    B2 \= 'Romeo'.
+
+% 2. The battalion led by Commander Gilbert isn't X-ray Battalion..
+clue2(T) :-
+    member([_, 'Gilbert', B1, _], T),
+    B1 \= 'X-ray'.
+
+% 4. Romeo Battalion will deploy sometime before 
+% the battalion led by Commander Charles..
+clue4(T) :-
+    member([M1, _, 'Romeo', _], T),
+    member([M2, 'Charles', _, _], T),
+    number(M1, N1),
+    number(M2, N2),
+    N1 < N2.
+
+% 6. The battalion led by Commander York won't deploy in February..
+clue6(T) :-
+    member([M1, 'York', _, _], T),
+    M1 \= 'February'.
+
+% 8. The battalion led by Commander Charles is either 
+% Romeo Battalion or the battalion deploying in March..
+clue8(T) :-
+    member([M1, 'Charles', B1, _], T),
+    (   B1 = 'Romeo' ; M1 = 'March').
+
+% 11. Of the battalion led by Commander Peters and Oscar Battalion,
+% one will deploy in March and the other will deploy in
+% May..
+clue11(T) :-
+    member([M1, 'Peters', _, _], T),
+    member([M2, _, 'Oscar', _], T),
+    (   M1 = 'March', M2 = 'May' ; M1 = 'May', M2 = 'March').
+
+% 1. The battalion led by Commander Gilbert will deploy 
+% 1 month after the group deploying to Quenray..
+clue1(T) :-
+    member([M1, 'Gilbert', _, _], T),
+    member([M2, _, _, 'Quenray'], T),
+    number(M1, N1),
+    number(M2, N2),
+    (   N2 + 1) =:= N1.
+
+% 3. The battalion led by Commander Charles will deploy to Quenray..
+clue3(T) :-
+    member([_, 'Charles', _, L1], T), 
+    L1 = 'Quenray'.
+
+% 5. The battalion deploying in May won't deploy to Zenmoto..
+clue5(T) :-
+    member(['May', _, _, L1], T),
+    L1 \= 'Zenmoto'.
+
+% 7. The battalion led by Commander Bauer will deploy to Jaorm..
+clue7(T) :-
+    member([_, 'Bauer', _, L1], T),
+    L1 = 'Jaorm'.
+
+% 10. Of the battalion led by Commander Gilbert and Tango Battalion,
+% one will deploy to Nyeos and the other will deploy in
+% May..
+clue10(T) :-
+    member([M1, 'Gilbert', _, D1], T),
+    member([M2, _, 'Tango', D2], T),
+    (   M1 = 'May', D2 = 'Nyeos' ; D1 = 'Nyeos', M2 = 'May').
+
 
 
 check :- 
--- veryhard.pl
+++ veryhard.pl
@@ -1,14 +1,131 @@
+% Samuel Fisher
+% CSE465
+% Project 1
+% Dr. Femiani
+% Due March 28, 2022
 
-%% I will not accept a submission with the T0D0 comments left in place!
+url("https://logic.puzzlebaron.com/pdf/B970MB.pdf").
 
-url("TODO: Replace this with the URL of the puzzle").
+solution([
+[1984,	'Miranda',	'Caprica',		'Trinidad'],
+[1985,	'Greg',		'Farralon',		'Barbados'],
+[1986,	'Ollie',	'Neptunia',		'Jamaica'],
+[1987,	'Andre',	'Baroness',		'Saint Lucia'],
+[1988,	'Eugene',	'Azure Seas',	'Grenada'],
+[1989,	'Cory',		'Trinity',		'Aruba']
+  ]).
 
-solution([["TODO: Replace this with the correct solution to the puzzle!"]]).
+names(['Miranda', 'Greg', 'Ollie', 'Andre', 'Eugene', 'Cory']).
+cruise(['Caprica', 'Farralon', 'Neptunia', 'Baroness', 'Azure Seas', 'Trinity']).
+destinations(['Trinidad', 'Barbados', 'Jamaica', 'Saint Lucia', 'Grenada', 'Aruba']).
 
-solve(T) :-
-	% TODO: Implement this to solve the puzzle. 
+solve(T) :- 
+    T = [ [1984, N1, C1, D1],
+          [1985, N2, C2, D2],
+          [1986, N3, C3, D3],
+          [1987, N4, C4, D4],
+          [1988, N5, C5, D5],
+          [1989, N6, C6, D6]
+        ],
+    names(Names), permutation(Names, [N1, N2, N3, N4, N5, N6]),
+    clue4(T),  % N, _, _
+    destinations(Destinations), permutation(Destinations, [D1, D2, D3, D4, D5, D6]),
+    clue5(T),  % N, _, D
+    clue8(T),  % N, _, D
+    clue9(T),  % N, _, D
+    clue10(T), % N, _, D
+    clue11(T), % N, _, D
+    cruise(Cruise), permutation(Cruise, [C1, C2, C3, C4, C5, C6]),
+    clue1(T),  % N, C, _
+    clue2(T),  % N, C, _
+    clue3(T),  % N, C, D
+    clue6(T),  % N, C, D
+    clue7(T),  % N, C, _
+    clue12(T), % N, C, D
+    clue13(T), % N, C, D
 	true.
 
+% 4. Cory wasn't on the 1988 cruise.
+clue4(T) :-
+   member([Y1, 'Cory', _, _], T),
+   Y1 \= 1988.
+
+% 5. Greg didn't go to Saint Lucia.
+clue5(T) :-
+   member([_, 'Greg', _, D1], T),
+   D1 \= 'Saint Lucia'.
+    
+% 8. Neither the traveler who took the 1985 cruise 
+% nor the person who went to Trinidad is Eugene.
+clue8(T) :-
+    member([1985, N1, _, D1], T),
+    member([Y2, N2, _, 'Trinidad'], T),
+    N1 \= 'Eugene', N2 \= 'Eugene',
+    D1 \= 'Trinidad', Y2 \= 1985.
+
+% 9. The person who took the 1987 cruise is either
+% Eugene or the person who went to Saint Lucia.
+clue9(T) :-
+    member([1987, N1, _, D1], T),
+    (   N1 = 'Eugene', D1 \= 'Saint Lucia' ; D1 = 'Saint Lucia', N1 \= 'Eugene').
+
+% 10. The person who took the 1985 cruise is either
+% the traveler who went to Barbados or Cory.
+clue10(T) :-
+    member([1985, N1, _, D1], T),
+    (   N1 = 'Cory', D1 \= 'Barbados' ; D1 = 'Barbados', N1 \= 'Cory').
+
+% 11. Miranda set sail 4 years before the person who went to Grenada.
+clue11(T) :-
+    member([Y1, 'Miranda', _, _], T),
+    member([Y2, _, _, 'Grenada'], T),
+    Y2 - 4 =:= Y1.
+
+% 1. Of the person who took the 1988 cruise and Ollie,
+% one took the Neptunia cruise and the other took the 
+% Azure Seas cruise.
+clue1(T) :-
+    member([1988, _, C1, _], T),
+    member([_, 'Ollie', C2, _], T),
+    (   C1 = 'Neptunia', C2 = 'Azure Seas' ; C1 = 'Azure Seas', C2 = 'Neptunia').
+
+% 2. Of the traveler who took the 1986 cruise and Andre, 
+% one took the Baroness cruise and the other took the Neptunia cruise.
+clue2(T) :-
+    member([1986, _, C1, _], T),
+    member([_, 'Andre', C2, _], T),
+    (   C1 = 'Baroness', C2 = 'Neptunia' ; C1 = 'Neptunia', C2 = 'Baroness').
+
+% 3. The traveler who took the Neptunia cruise set sail 3 years 
+% before the traveler who went to Aruba.
+clue3(T) :-
+    member([Y1, _, 'Neptunia', _], T),
+    member([Y2, _, _, 'Aruba'], T),
+    Y2 - 3 =:= Y1.
+
+% 6. Miranda is either the person who went to 
+% Jamaica or the traveler who took the Caprica cruise.
+clue6(T) :-
+    member([_, 'Miranda', C1, D1], T),
+    (   C1 = 'Caprica', D1 \= 'Jamaica' ; D1 = 'Jamaica', C1 \= 'Caprica').
+
+% 7. Cory wasn't on the Farralon cruise.
+clue7(T) :-
+    member([_, 'Cory', C1, _], T),
+    C1 \= 'Farralon'.
+
+% 12. The person who went to Saint Lucia set sail 3 
+% years after the person who took the Caprica cruise.
+clue12(T) :-
+    member([Y1, _, _, 'Saint Lucia'], T),
+    member([Y2, _, 'Caprica', _], T),
+    Y2 + 3 =:= Y1.
+
+% 13. Cory is either the person who took the Azure Seas
+% cruise or the person who went to Aruba.
+clue13(T) :-
+    member([_, 'Cory', C1, D1], T),
+    (   C1 = 'Azure Seas', D1 \= 'Aruba' ; D1 = 'Aruba', C1 \= 'Azure Seas').
 
 
 check :- 
